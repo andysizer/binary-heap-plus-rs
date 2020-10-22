9fd78b22b425c5d4f02aa5d0d38de571ffd5875c
Andy Sizer <andysizer@gmail.com>
2020-10-22 15:51:48 +0700

    Work on comments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,48 @@
-//! This crate provides `GHeap` which is derived from 2 sources:
+//! This crate provides `GHeap` which implements a n-ary heap in which subtrees containing k items are laid out contiguously 
+//! in a 'chunk'. `GHeap` can be used to implement a [Priority Queue](https://www.wikiwand.com/en/Priority_queue#:~:text=In%20computer%20science%2C%20a%20priority,an%20element%20with%20low%20priority.)
+//! in a similar fashion to `std::collections::BinaryHeap`.
+//! 
+//! This implementation was motivated by the article ['You're Doing It Wrong'](https://queue.acm.org/detail.cfm?id=1814327) which
+//! introduces the [B-heap](https://www.wikiwand.com/en/B-heap). The main idea is that laying out subtrees contiguously will tend to increase
+//! locality when the heap is traversed using the parent-child relation and that he benefits of increased locality will tend to outweigh
+//! the increased cost of computing the parent-child relation when memory is under pressure due to the cost of paging.
+//! 
+//! The `benches` directory contains benchmarks to test this idea. These appear to show that most of the time any improvement shown by `GHeap` over 
+//! `BinaryHeap` is due to 'fanout' (n-ariness) rather than chunking. However there appear to be 'sweet spots' where a judicious choice of fanout and
+//! pagechunk size for items of a given size can show significant improvement in performance, implying that for certain use cases GHeap might be a 
+//! reasonable replacement for `std::collections::BinaryHeap` (or a simpler implementation of an n-ary heap).
+//! 
+//! Insertion and popping the largest element from a GHeap with a fanout (arity) of 'd' have `O(log n / log d)` 
+//! time complexity. Checking the largest element is `O(1)`. Converting 
+//! a vector to a GHeap can be done in-place, and has `O(n)` complexity. A GHeap can also be
+//! converted to a sorted vector in-place, allowing it to be used for an `O(n
+//! log n)` in-place heapsort.
+//!
+//! 
+//! This implementation is derived from:
 //! 1. [binary-heap-plus](https://github.com/sekineh/binary-heap-plus-rs)
 //!    which is itself forked from the version of BinaryHeap in the Rust standard library.
 //! 2. The C++ version of [gheap](https://github.com/valyala/gheap.git)
 //!
-//! `GHeap` implements a n-ary heap in which subtrees are laid out contiguously in a single 'page' and 
-//! can be used as a [Priority Queue](https://www.wikiwand.com/en/Priority_queue#:~:text=In%20computer%20science%2C%20a%20priority,an%20element%20with%20low%20priority.). 
-//! It is motivated by the article ['You're Doing It Wrong'](https://queue.acm.org/detail.cfm?id=1814327) which
-//! introduces the [B-heap](https://www.wikiwand.com/en/B-heap).
-//! 
 //! The main difference between a binary heap and a B-heap are the equations used
-//! to compute the indices of parent and child nodes in the heap. `GHeap` abstracts these computations out into
-//! a trait: `Indexer`. The primary reason for this is Rust's lack of support
-//! for const generics (i.e. rustc will reject 'GHeap<i32, 4, 2>'). 
-//! This may change: see [Shipping Const Generics in 2020](https://without.boats/blog/shipping-const-generics/).
+//! to compute the indices of parent and child nodes in the underlying representatoin of the heap. `GHeap` abstracts these computations out into
+//! a trait: `HeapIndexer`. The primary reason for this is Rust's lack of support for `const generics` in stable builds (i.e. rustc will reject 'GHeap<i32, 4, 2>'). 
+//! This may change at some point in the not too distant future (see [Shipping Const Generics in 2020](https://without.boats/blog/shipping-const-generics/)) as
+//! usable implementation of `const generics` is now available in nightly builds.
 //! 
-//! GHeap has three generic type parameters:
-//! 1. T - the type contained in the heap.
-//! 2. C - the type of the comparator used to order the heap
-//! 3. I - the type of the indexer used to layout the heap
+//! The additional features that `GHeap` has over and above those of `std::collections::BinaryHeap` are due to [binary-heap-plus](https://github.com/sekineh/binary-heap-plus-rs)
+//! and include:
 //! 
-//! Features include:
-//! * Heaps other than max heap e.g. min heap and heaps constructed using custom comparators
-//! * Optional `serde` feature.
+//! * Heaps other than max heap e.g. min heap and heaps constructed using custom comparators.
+//! * Support for serialization and deserialization if `serde` is enabled.
 //!
-//! A utility macro 'def_indexer' is provided to make defining simple indexers trivial.
+//! A utility macro 'def_indexer' is provided to make defining indexers with different 'fanouts' and 'pagechunks' trivial.
 //! 
 //! # Quick start
 //!
 //! ## Max/Min Heap
 //!
-//! For max heap, `GHeap::from_vec()` is the most versatile way to create a heap.
+//! `GHeap::from_vec()` is the easiest way to create a max heap from a vector.
 //!
 //! ```rust
 //!     use gheap::*;
@@ -43,7 +56,7 @@
 //!     assert_eq!(h.pop(), Some(41));
 //! ```
 //!
-//! Min heap is similar, but requires type annotation.
+//! A min heap can be created in a similar fashion through type annotation.
 //!
 //! ```rust
 //!     use gheap::*;
@@ -59,7 +72,7 @@
 //!
 //! ## Custom Heap
 //!
-//! For custom heap, `GHeap::from_vec_cmp()` works in a similar way to max/min heap. The only difference is that you add the comparator closure with apropriate signature.
+//! `GHeap::from_vec_cmp()` takes a comparator closure to produce a custom heap.
 //!
 //! ```rust
 //!     use gheap::*;
@@ -73,22 +86,21 @@
 //! ```
 //! ## Custom Indexer
 //!
-//! For a max heap with a custom indexer, `GHeap::from_vec_indexer()` . 
+//! `GHeap::from_vec_indexer()` constructs a heap with a custom indexer.
 //!
 //! ```rust
 //!     use gheap::*;
 //!
-//!     // define ThreeTwoIndexer to be an Indexer that uses a fanout of 3 and 2 page chunks.
+//!     // define ThreeTwoIndexer to be a HeapIndexer that uses a fanout of 3 and pagechunk size of 2.
 //!     def_indexer!(ThreeTwoIndexer, 3,2);
-//!     // Supply an explicit indexer. As with 'GHeap::from_vec' requires type annotation.
-//!     let mut h: GHeap<i32, MaxComparator,ThreeTwoIndexer> = GHeap::from_vec_indexer(
+//!     let mut h = GHeap::from_vec_indexer(
 //!         vec![1,5,3], 
-//!         ThreeTwoIndexer{}, // Create the indexer
+//!         ThreeTwoIndexer{}, // supply the indexer
 //!     );
 //!     assert_eq!(h.pop(), Some(5));
 //! ```
 //!
-//! # Constructers
+//! # Construction
 //!
 //! ## Generic methods to create different kind of heaps from initial `vec` data.
 //!
@@ -126,7 +138,7 @@
 //! // max heap with a customized indexer
 //! def_indexer!(ThreeTwoIndexer, 3,2);
 //! let idxer = ThreeTwoIndexer{}; 
-//! let mut heap: GHeap<i32, MaxComparator,ThreeTwoIndexer> = GHeap::from_vec_indexer(
+//! let mut heap = GHeap::from_vec_indexer(
 //!     vec![1,5,3], 
 //!     idxer
 //! );
@@ -144,10 +156,161 @@
 //! ## Dedicated methods to create different kind of heaps
 //!
 //! * `GHeap::new()` creates a max heap.
+//! * `GHeap::with_capacity()` creates a max heap with the requested capacity.
+//! * `GHeap::new_indexer()` creates a max heap with the supplied indexer.
+//! * `GHeap::with_capacity_indexer()` creates a max heap with the requested capacity and supplied indexer.
 //! * `GHeap::new_min()` creates a min heap.
-//! * `GHeap::new_by()` creates a heap sorted by the given closure.
-//! * `GHeap::new_by_key()` creates a heap sorted by the key generated by the given closure.
+//! * `GHeap::new_min_indexer()` creates a min heap with the supplied indexer.
+//! * `GHeap::with_capacity_min()` creates a min heap with the requested capacity.
+//! * `GHeap::with_capacity_min_indexer()` creates a min heap with the requested capacity and supplied indexer.
+//! * `GHeap::new_by()` creates a heap ordered by the given closure.
+//! * `GHeap::new_by_indexer()` creates a heap with the supplied indexer and ordered by the given closure.
+//! * `GHeap::with_capacity_by()` creates a heap with the requested capacity ordered by the given closure.
+//! * `GHeap::with_capacity_by_indexer()` creates a heap with the requested capacity and supplied indexer, ordered by the given closure.
+//! * `GHeap::new_by_key()` creates a heap ordered by the key generated by the given closure.
+//! * `GHeap::new_by_key_indexer()` creates a heap with the supplied indexer and ordered by the key generated by the given closure.
+//! * `GHeap::with_capacity_by_key()` creates a heap with the requested capacity ordered by the key generated by the given closure.
+//! * `GHeap::with_capacity_by_key_indexer()` creates a heap with the requested capacity and supplied indexer, ordered by the key generated by the given closure.
 //!
+//! 
+//! # Example
+//! 
+//! A priority queue implemented with a GHeap.
+//!
+//! This example implements [Dijkstra's algorithm][dijkstra]
+//! to solve the [shortest path problem][sssp] on a [directed graph][dir_graph].
+//! It shows how to use [`GHeap`] with custom types.
+//!
+//! [dijkstra]: http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+//! [sssp]: http://en.wikipedia.org/wiki/Shortest_path_problem
+//! [dir_graph]: http://en.wikipedia.org/wiki/Directed_graph
+//! [`GHeap`]: struct.GHeap.html
+//!
+//! ```
+//! use std::cmp::Ordering;
+//! use gheap::*;
+//! use std::usize;
+//!
+//! #[derive(Copy, Clone, Eq, PartialEq)]
+//! struct State {
+//!     cost: usize,
+//!     position: usize,
+//! }
+//!
+//! // The priority queue depends on `Ord`.
+//! // Explicitly implement the trait so the queue becomes a min-heap
+//! // instead of a max-heap.
+//! impl Ord for State {
+//!     fn cmp(&self, other: &State) -> Ordering {
+//!         // Notice that the we flip the ordering on costs.
+//!         // In case of a tie we compare positions - this step is necessary
+//!         // to make implementations of `PartialEq` and `Ord` consistent.
+//!         other.cost.cmp(&self.cost)
+//!             .then_with(|| self.position.cmp(&other.position))
+//!     }
+//! }
+//!
+//! // `PartialOrd` needs to be implemented as well.
+//! impl PartialOrd for State {
+//!     fn partial_cmp(&self, other: &State) -> Option<Ordering> {
+//!         Some(self.cmp(other))
+//!     }
+//! }
+//!
+//! // Each node is represented as an `usize`, for a shorter implementation.
+//! struct Edge {
+//!     node: usize,
+//!     cost: usize,
+//! }
+//!
+//! // Dijkstra's shortest path algorithm.
+//!
+//! // Start at `start` and use `dist` to track the current shortest distance
+//! // to each node. This implementation isn't memory-efficient as it may leave duplicate
+//! // nodes in the queue. It also uses `usize::MAX` as a sentinel value,
+//! // for a simpler implementation.
+//! fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal: usize) -> Option<usize> {
+//!     // dist[node] = current shortest distance from `start` to `node`
+//!     let mut dist: Vec<_> = (0..adj_list.len()).map(|_| usize::MAX).collect();
+//!
+//!     let mut heap = GHeap::new();
+//!
+//!     // We're at `start`, with a zero cost
+//!     dist[start] = 0;
+//!     heap.push(State { cost: 0, position: start });
+//!
+//!     // Examine the frontier with lower cost nodes first (min-heap)
+//!     while let Some(State { cost, position }) = heap.pop() {
+//!         // Alternatively we could have continued to find all shortest paths
+//!         if position == goal { return Some(cost); }
+//!
+//!         // Important as we may have already found a better way
+//!         if cost > dist[position] { continue; }
+//!
+//!         // For each node we can reach, see if we can find a way with
+//!         // a lower cost going through this node
+//!         for edge in &adj_list[position] {
+//!             let next = State { cost: cost + edge.cost, position: edge.node };
+//!
+//!             // If so, add it to the frontier and continue
+//!             if next.cost < dist[next.position] {
+//!                 heap.push(next);
+//!                 // Relaxation, we have now found a better way
+//!                 dist[next.position] = next.cost;
+//!             }
+//!         }
+//!     }
+//!
+//!     // Goal not reachable
+//!     None
+//! }
+//!
+//! fn main() {
+//!     // This is the directed graph we're going to use.
+//!     // The node numbers correspond to the different states,
+//!     // and the edge weights symbolize the cost of moving
+//!     // from one node to another.
+//!     // Note that the edges are one-way.
+//!     //
+//!     //                  7
+//!     //          +-----------------+
+//!     //          |                 |
+//!     //          v   1        2    |  2
+//!     //          0 -----> 1 -----> 3 ---> 4
+//!     //          |        ^        ^      ^
+//!     //          |        | 1      |      |
+//!     //          |        |        | 3    | 1
+//!     //          +------> 2 -------+      |
+//!     //           10      |               |
+//!     //                   +---------------+
+//!     //
+//!     // The graph is represented as an adjacency list where each index,
+//!     // corresponding to a node value, has a list of outgoing edges.
+//!     // Chosen for its efficiency.
+//!     let graph = vec![
+//!         // Node 0
+//!         vec![Edge { node: 2, cost: 10 },
+//!              Edge { node: 1, cost: 1 }],
+//!         // Node 1
+//!         vec![Edge { node: 3, cost: 2 }],
+//!         // Node 2
+//!         vec![Edge { node: 1, cost: 1 },
+//!              Edge { node: 3, cost: 3 },
+//!              Edge { node: 4, cost: 1 }],
+//!         // Node 3
+//!         vec![Edge { node: 0, cost: 7 },
+//!              Edge { node: 4, cost: 2 }],
+//!         // Node 4
+//!         vec![]];
+//!
+//!     assert_eq!(shortest_path(&graph, 0, 1), Some(1));
+//!     assert_eq!(shortest_path(&graph, 0, 3), Some(3));
+//!     assert_eq!(shortest_path(&graph, 3, 0), Some(7));
+//!     assert_eq!(shortest_path(&graph, 0, 4), Some(5));
+//!     assert_eq!(shortest_path(&graph, 4, 0), None);
+//! }
+//! ```
+
 
 #[macro_use] mod gheap;
 pub use crate::gheap::*;
